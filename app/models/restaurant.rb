2906efc03baa9388933875f0b4a0307eb9657097
guillermo varela <guillovarela16@gmail.com>
2019-07-03 16:35:46 -0300

    add has many to model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+  has_many :reviews
   validates :name, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
